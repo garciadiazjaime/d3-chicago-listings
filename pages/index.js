e491d05a01589d4d1e606490920f6cd883e16676
Jaime Garcia Diaz <jgarciadiaz@groupon.com>
2018-07-21 20:17:04 -0500

    feat: display zipCode and one entry

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,53 @@ import Head from 'next/head'
 
 
 export default class extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
   
   componentDidMount() {
     console.log('componentDidMount')
-    d3.csv('/static/db.csv', data => {
-      console.log('data', data);
-    })
+    const priceByZipCode = {}
+    d3.csv('/static/1BD_Listing_Chicago_Zip.csv')
+      .then(data => {
+        Object.keys(data).forEach(key => {
+          let zipCode = ''
+          let prices = []
+          if (!isNaN(key)) {
+            Object.keys(data[key]).forEach(date => {
+              if (date === 'Zip Code') {
+                zipCode = data[key][date]
+              } else {
+                prices.push({
+                  date,
+                  price: data[key][date]
+                })
+              }
+            })
+            priceByZipCode[zipCode] = prices
+          }
+        })
+        const zipCodes = Object.keys(priceByZipCode)
+
+        d3.select("body")
+          .selectAll("p")
+          .data(zipCodes)
+          .enter().append("p")
+          .text(zipCode => {
+            return `${zipCode}: ${JSON.stringify(priceByZipCode[zipCode][0])}`
+          });
+
+        this.setState({
+          priceByZipCode
+        })
+      })
   }
 
   render() {
+    const { priceByZipCode } = this.state
+    console.log('priceByZipCode', priceByZipCode)
     return (
       <div>
         <Head>
